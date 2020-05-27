3e2de5db19da73d4590dfd8ca302186005d4c68b
Joseph Serunjogi <joeseggie@gmail.com>
2020-05-28 00:20:41 +0300

    Test tables with sticky columns and headers in react
    Tested packages react-table-sticky and react-stick-table on top of react-table
    and it looks like react-sticky-table is the easiest of the two.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,414 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {StickyTable, Row, Cell} from 'react-sticky-table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div style={{width: '100%', height: '400px'}}>
+                    <StickyTable leftStickyColumnCount={2}>
+                        <Row>
+                            <Cell>Header 1</Cell>
+                            <Cell>Header 2</Cell>
+                            <Cell>Header 1</Cell>
+                            <Cell>Header 2</Cell>
+                            <Cell>Header 1</Cell>
+                            <Cell>Header 2</Cell>
+                            <Cell>Header 1</Cell>
+                            <Cell>Header 2</Cell>
+                            <Cell>Header 1</Cell>
+                            <Cell>Header 2</Cell>
+                            <Cell>Header 2</Cell>
+                            <Cell>Header 1</Cell>
+                            <Cell>Header 2</Cell>
+                            <Cell>Header 1</Cell>
+                            <Cell>Header 2</Cell>
+                            <Cell>Header 1</Cell>
+                            <Cell>Header 2</Cell>
+                            <Cell>Header 1</Cell>
+                            <Cell>Header 2</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                        <Row>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                            <Cell>Cell 2</Cell>
+                            <Cell>Cell 1</Cell>
+                        </Row>
+                    </StickyTable>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default App;
